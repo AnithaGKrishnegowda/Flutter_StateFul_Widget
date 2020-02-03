77803530101973c557dbeeeb0e871e2f2b452818
AnithaGKrishnegowda <anithagk777@gmail.com>
2020-02-03 13:16:27 +0530

    Flutter Practicing StateFull Widget

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(new application());
+}
+class application extends StatefulWidget {
+  @override
+  _applicationState createState() => _applicationState();
+}
+
+class _applicationState extends State<application> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
